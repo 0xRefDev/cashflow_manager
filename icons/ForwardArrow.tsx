fb095904b90488c013ee542a24c24aae1b314cba
0xRefDev <refh4ck.dev@gmail.com>
2026-03-27 16:38:18 -0400

    feat: get started svg arrow icon

---
+++ icons/ForwardArrow.tsx
@@ -0,0 +1,16 @@
+export function ForwardArrow(props: React.SVGProps<SVGSVGElement>) {
+  return (<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    {...props}
+    className="w-[1.1em] transition-transform duration-300 text-[#32844e] group-hover:translate-x-[0.1em]"
+  >
+    <path fill="none" d="M0 0h24v24H0z"></path>
+    <path
+      fill="currentColor"
+      d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+    ></path>
+  </svg>);
+}
