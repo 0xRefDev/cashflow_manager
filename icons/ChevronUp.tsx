d135cb87ecbf3e1127cbb8f9e38c240afd3c9c36
0xRefDev <refh4ck.dev@gmail.com>
2026-03-27 16:38:04 -0400

    feat: svg arrow up icon

---
+++ icons/ChevronUp.tsx
@@ -0,0 +1,3 @@
+export function ChevronUp(props: React.SVGProps<SVGSVGElement>) {
+  return (<svg xmlns="http://www.w3.org/2000/svg" width={28} height={28} viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m6 15l6-6l6 6"></path></svg>);
+}
