39712faf3f8994beac4c20aea6daa5cd62d395df
0xRefDev <refh4ck.dev@gmail.com>
2026-03-27 16:37:41 -0400

    feat: burger menu icon

---
+++ icons/BurgerMenu.tsx
@@ -0,0 +1,3 @@
+export function BurgerMenu(props: React.SVGProps<SVGSVGElement>) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 6h18M3 12h18M3 18h18"></path></svg>);
+}
