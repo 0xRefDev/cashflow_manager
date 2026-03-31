f2aba995bef2cd8ac5b9e0f4bb1cb536e0c19f1d
0xRefDev <refh4ck.dev@gmail.com>
2026-03-31 16:30:46 -0400

    feat: add svg icons

---
+++ icons/Reports.tsx
@@ -0,0 +1,3 @@
+export function Reports(props: React.SVGProps<SVGSVGElement>) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 16 16" {...props}><path fill="none" stroke="currentColor" strokeLinejoin="round" d="M7.563 1.545H2.5v10.91h9V5.364M7.563 1.545L11.5 5.364M7.563 1.545v3.819H11.5m-7 9.136h9v-7M4 7.5h6M4 5h2m-2 5h6" strokeWidth={1}></path></svg>);
+}
