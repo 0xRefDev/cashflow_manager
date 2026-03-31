4c9cd7824c7a40ba5328f3011175efce08a5ad86
0xRefDev <refh4ck.dev@gmail.com>
2026-03-31 16:32:19 -0400

    fix: adjust section viewport height

--- views/Hero.tsx
+++ views/Hero.tsx
@@ -10,7 +10,7 @@ export function Hero() {
   return (
     <section
       id="hero"
-      className="min-h-screen h-[150dvh] w-full relative scroll-mt-20 flex justify-center pt-24 lg:pt-32"
+      className="min-h-screen h-dvh lg:h-[165dvh] 2xl:h-[115dvh] w-full relative flex justify-center pt-24 lg:pt-32"
     >
 
       <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:20px_40px] mask-[radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
@@ -26,7 +26,7 @@ export function Hero() {
             <span className="text-landing-primary">Money</span>
           </h1>
 
-          <p className="text-sm lg:text-lg text-gray-400 leading-relaxed mt-1 lg:mt-0 max-w-xs lg:max-w-md text-pretty">
+          <p className="text-sm lg:text-lg text-gray-300 leading-relaxed mt-1 lg:mt-0 max-w-xs lg:max-w-md text-pretty">
             Navigate your financial journey with editorial clarity. Track every
             cent, forecast your growth, and achieve absolute fiscal sovereignty.
           </p>
@@ -35,7 +35,7 @@ export function Hero() {
             <Button className="relative bg-[#32844e] text-white font-medium text-[17px] px-4 py-[0.35em] pl-5 h-[2.8em] rounded-[0.9em] flex items-center overflow-hidden cursor-pointer ">
               <span className="mr-10">Get started</span>
               <div
-                className="absolute right-[0.3em] bg-white h-[2.2em] w-[2.2em] rounded-[0.7em] flex items-center justify-center transition-all duration-300 active:scale-95"
+                className="absolute right-[0.3rem] bg-white h-[2.2rem] w-[2.2rem] rounded-[0.7rem] flex items-center justify-center transition-all duration-300 active:scale-95"
               >
                 <ForwardArrow />
               </div>
@@ -45,11 +45,15 @@ export function Hero() {
             </p>
           </div>
 
-          <div className="relative w-full lg:w-4xl lg:mt-2">
+          <div className="relative w-full max-w-[1280px] lg:w-4xl lg:mt-2">
             <Image
               src={DashImage}
               alt="CashFlow Dashboard Preview"
-              className="w-full px-2 h-auto object-cover absolute top-2 lg:top-0 left-1/2 -translate-x-1/2 drop-shadow-xl drop-shadow-black/60 select-none"
+              className="w-full px-2 h-auto object-cover drop-shadow-xl drop-shadow-black/60 select-none max-w-[1280px]"
+              style={{
+                maskImage: 'linear-gradient(to bottom, black 30%, rgba(0,0,0,0) 85%)',
+                WebkitMaskImage: 'linear-gradient(to bottom, black 30%, rgba(0,0,0,0) 85%)',
+              }}
               priority
             />
           </div>
