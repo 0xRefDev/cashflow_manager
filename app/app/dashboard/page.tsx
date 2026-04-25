beff089dfc6bb222fac49a0582ab34f9537945e6
0xRefDev <refh4ck.dev@gmail.com>
2026-04-24 20:22:14 -0400

    feat: add all pages routes of the app

---
+++ app/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <section className="h-full p-6 text-white">
+      Dashboard
+    </section>
+  );
+}
---
+++ app/app/undev/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <section className="h-full p-6 text-white">
+      {/* content */}
+    </section>
+  );
+}
