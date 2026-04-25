62b6d819eed68ba80f338865f3734138ef3dd64e
0xRefDev <refh4ck.dev@gmail.com>
2026-04-24 20:25:18 -0400

    fix: add type and disabled props support

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,5 @@
-export function Button ({ children, className, onClick, type, id }: { children: React.ReactNode, className?: string, onClick?: () => void, type?: "button" | "submit", id?: string }) {
+export function Button ({ children, className, onClick, type, id, disabled }: { children: React.ReactNode, className?: string, onClick?: () => void, type?: "button" | "submit", id?: string, disabled?: boolean }) {
   return (
-    <button onClick={onClick} type={type} id={id} className={`cursor-pointer ${className}`}>{children}</button>
+    <button onClick={onClick} type={type} id={id} disabled={disabled} className={`cursor-pointer ${className}`}>{children}</button>
   )
 }
