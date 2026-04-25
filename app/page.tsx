fdb86d1d20e19be01c7237a7fd74abdab55a8409
0xRefDev <refh4ck.dev@gmail.com>
2026-04-24 20:19:00 -0400

    fix: delete fragments

--- app/page.tsx
+++ app/page.tsx
@@ -11,18 +11,20 @@ import { Footer } from "@/components/landing/Footer";
 
 export default function Home() {
   return (
-    <main className="relative max-w-[1920px] mx-auto">
-      <Header />
+    <>
+      <main className="relative max-w-[1920px] mx-auto">
+        <Header />
 
-      <section className="flex flex-col">
-        <Hero />
-        <HowItWork />
-        <Features />
-      </section>
-      <Contact />
-      <Footer />
+          <section className="flex flex-col">
+            <Hero />
+            <HowItWork />
+            <Features />
+          </section>
+          <Contact />
+          <Footer />
 
-      <a href="#hero" className="fixed bottom-5 right-5 bg-[#88ec7b] text-black px-0.5 py-0.5 rounded-full hover:bg-[#74d469] transition-colors duration-300 z-1000"><ChevronUp /></a>
-    </main>
+          <a href="#hero" className="fixed bottom-5 right-5 bg-[#88ec7b] text-black px-0.5 py-0.5 rounded-full hover:bg-[#74d469] transition-colors duration-300 z-1000"><ChevronUp /></a>
+      </main>
+    </>
   );
 }
