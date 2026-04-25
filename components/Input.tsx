c54b0db61acbefb68af1fad1f0f45e2f61a0c631
0xRefDev <refh4ck.dev@gmail.com>
2026-04-24 20:26:08 -0400

    feat: add ref, resume props and autocomplete prop

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,13 +1,18 @@
-export function Input({ type, placeholder, className, id, value, onChange, name }: { type: string, placeholder: string, className?: string, id: string, name: string, value: string, onChange: (e: React.ChangeEvent<HTMLInputElement>) => void }) {
-  return (
-    <input
-      type={type}
-      placeholder={placeholder}
-      className={`w-full bg-[#111111] border border-white/5 rounded-xl py-2 px-4 text-white placeholder-gray-500 outline-none hover:border-white/10 focus:border-[#4ade80]/50 focus:ring-1 focus:ring-[#4ade80]/20 transition-all duration-300 ${className}`}
-      id={id}
-      name={name}
-      value={value}
-      onChange={onChange}
-    />
-  )
-}
+import { forwardRef } from "react"
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        autoComplete="false"
+        className={`w-full bg-[#111111] border border-white/5 rounded-xl py-2 px-4 text-white placeholder-gray-500 outline-none hover:border-white/10 focus:border-[#4ade80]/50 focus:ring-1 focus:ring-[#4ade80]/20 transition-all duration-300 ${className}`}
+        {...props}
+      />
+    )
+  }
+)
+
+Input.displayName = "Input"
