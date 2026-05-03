ba500b0b4ccb0455e127c163fbea38e0496db3cf
0xRefDev <refh4ck.dev@gmail.com>
2026-05-02 20:03:51 -0400

    feat: add turboPack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    resolveAlias: {},
+    rules: {},
+  },
+  typescript: {
+    ignoreBuildErrors: false
+  },
 };
 
 export default nextConfig;
