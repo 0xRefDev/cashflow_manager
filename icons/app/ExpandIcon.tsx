0b3fc1714e5b0f7b3fccc345248201e83775fc96
0xRefDev <refh4ck.dev@gmail.com>
2026-05-05 17:49:54 -0400

    feat: add new app icons

---
+++ icons/app/ExpandIcon.tsx
@@ -0,0 +1,3 @@
+export function ExpandIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (<svg xmlns="http://www.w3.org/2000/svg" {...props} height={24} width={24} viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M9 9 4 4m0 0v4m0-4h4m7 5 5-5m0 0v4m0-4h-4M9 15l-5 5m0 0v-4m0 4h4m7-5 5 5m0 0v-4m0 4h-4" strokeWidth={1.5}/></svg>);
+}
