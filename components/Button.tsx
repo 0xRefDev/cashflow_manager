2976fdbb2197a094cdd328fc2a0f67ac46cff167
0xRefDev <refh4ck.dev@gmail.com>
2026-05-05 17:48:33 -0400

    fix: add aria labels, title

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,16 @@
-export function Button ({ children, className, onClick, type, id, disabled }: { children: React.ReactNode, className?: string, onClick?: () => void, type?: "button" | "submit", id?: string, disabled?: boolean }) {
+export function Button({ children, className, onClick, type, id, disabled, title, ariaLabel, ariaPressed, ariaExpanded }: {
+  children: React.ReactNode,
+  className?: string,
+  onClick?: () => void,
+  type?: "button" | "submit",
+  id?: string,
+  disabled?: boolean,
+  title?: string,
+  ariaLabel?: string,
+  ariaPressed?: boolean,
+  ariaExpanded?: boolean
+}) {
   return (
-    <button onClick={onClick} type={type} id={id} disabled={disabled} className={`cursor-pointer ${className}`}>{children}</button>
+    <button onClick={onClick} type={type} id={id} disabled={disabled} title={title} aria-label={ariaLabel} aria-pressed={ariaPressed} aria-expanded={ariaExpanded} className={`cursor-pointer ${className}`}>{children}</button>
   )
 }
