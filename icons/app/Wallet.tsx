b11aa17f5b90f6d0bfc7ac20cfa1b9b2e9c0b4cc
0xRefDev <refh4ck.dev@gmail.com>
2026-07-02 18:03:54 -0400

    feat: add new wallet icon to aside

---
+++ icons/app/Wallet.tsx
@@ -0,0 +1,6 @@
+export function Wallet(props: React.SVGProps<SVGSVGElement>) {
+  return (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" {...props} viewBox="0 0 512 512"><path fill="none" d="M0 0h512v512H0z"/><rect width="416" height="288" x="48" y="144" fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="32" rx="48" ry="48"/><path fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="32" d="M411.36 144v-30A50 50 0 0 0 352 64.9L88.64 109.85A50 50 0 0 0 48 159v49"/><path fill="currentColor" d="M368 320a32 32 0 1 1 32-32 32 32 0 0 1-32 32"/></svg>
+
+
+  );
+}
