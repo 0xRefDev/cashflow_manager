c06afbc5c858852c5f83925019270639205dd9b9
0xRefDev <refh4ck.dev@gmail.com>
2026-07-15 13:41:09 -0400

    feat: add decimals to currencies schema

--- models/Currencies.ts
+++ models/Currencies.ts
@@ -3,6 +3,7 @@ import mongoose, { Schema } from "mongoose";
 const Currencies = new Schema({
   name: { type: String, required: true },
   symbol: { type: String, required: true },
+  decimals: { type: Number, required: true, default: 2 },
 }, { timestamps: true });
 
 export default mongoose.models.Currencies || mongoose.model("Currencies", Currencies);
