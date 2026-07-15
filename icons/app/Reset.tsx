321a1e673b3dd10c182677783ce9749798fe611f
0xRefDev <refh4ck.dev@gmail.com>
2026-07-15 13:35:29 -0400

    feat: reset icon

---
+++ icons/app/Reset.tsx
@@ -0,0 +1,4 @@
+export function Reset(props: React.SVGProps<SVGSVGElement>) {
+  return (<svg xmlns="http://www.w3.org/2000/svg" {...props} height={24} width={24} viewBox="0 0 21 21"><path fill="none" d="M0 0h21v21H0z"/><g fill="none" fillRule="evenodd" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"><path d="M3.578 6.487A8 8 0 1 1 2.5 10.5"/><path d="M7.5 6.5h-4v-4"/></g></svg>
+);
+}
