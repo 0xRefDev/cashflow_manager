dfb2a63cfc20e868a31067df41ee40386e3fe865
0xRefDev <refh4ck.dev@gmail.com>
2026-07-15 13:35:19 -0400

    feat: trash icon

---
+++ icons/app/Trash.tsx
@@ -0,0 +1,3 @@
+export function Trash(props: React.SVGProps<SVGSVGElement>) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" {...props} height={24} width={24} viewBox="0 0 24 24"><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}><path d="M3 6h18" /><path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" /><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /><path d="M10 11v6" /><path d="M14 11v6" /></g></svg>);
+}
