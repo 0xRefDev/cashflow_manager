7bc6cd1f657875c13c8a24dc7987278438b4e821
0xRefDev <refh4ck.dev@gmail.com>
2026-07-17 19:09:58 -0400

    feat: add spark icons

---
+++ icons/Sparks.tsx
@@ -0,0 +1,5 @@
+export function Sparks(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" {...props} height={24} width={24} viewBox="0 0 22 22"><path fill="#6e9bff" d="m18 8-1.25-2.75L14 4l2.75-1.25L18 0l1.25 2.75L22 4l-2.75 1.25zm0 14-1.25-2.75L14 18l2.75-1.25L18 14l1.25 2.75L22 18l-2.75 1.25zM8 19l-2.5-5.5L0 11l5.5-2.5L8 3l2.5 5.5L16 11l-5.5 2.5zm0-4.85L9 12l2.15-1L9 10 8 7.85 7 10l-2.15 1L7 12zM8 11"/></svg>
+  );
+}
