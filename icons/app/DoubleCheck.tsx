98614246d114770ef2864348ecaf9efcbd0a40fd
0xRefDev <refh4ck.dev@gmail.com>
2026-07-17 19:10:08 -0400

    feat: add double check icon

---
+++ icons/app/DoubleCheck.tsx
@@ -0,0 +1,6 @@
+export function DoubleCheck(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" {...props} width={24} height={24} viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m2 12 5.25 5 2.625-3M8 12l5.25 5L22 7m-6 0-3.5 4"/></svg>
+
+  );
+}
