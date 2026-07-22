09bc0a2dee0e4efbfd74ae95bce6a66b722dc119
0xRefDev <refh4ck.dev@gmail.com>
2026-07-22 13:35:41 -0400

    fix: change motion/react library to framer motion library

--- components/app/AnimatedArea.tsx
+++ components/app/AnimatedArea.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 
 export function AnimatedArea({ children }: { children: React.ReactNode }) {
   return (
